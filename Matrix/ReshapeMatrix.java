b3d2adca9924f47c12cc23024cc9821ac2a0d7a1
Piyush Shah <piyush720shah@gmail.com>
2026-07-02 21:16:00 +0530

    Add Matrix Reshape Program

---
+++ Matrix/ReshapeMatrix.java
@@ -0,0 +1,32 @@
+class ReshapeMatrix {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        if(m * n != r * c) {
+            return mat;
+        }
+
+        int[][] ans = new int[r][c];
+
+        for (int i = 0; i < m * n; i++) {
+            ans[i / c][i % c] = mat[i / n][i % n];
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        ReshapeMatrix solution = new ReshapeMatrix();
+        int[][] mat = {{1, 2}, {3, 4}};
+        int r = 1;
+        int c = 4;
+        int[][] reshapedMatrix = solution.matrixReshape(mat, r, c);
+
+        for(int i = 0; i < reshapedMatrix.length; i++) {
+            for(int j = 0; j < reshapedMatrix[0].length; j++) {
+                System.out.print(reshapedMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
