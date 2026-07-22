9a0bbf4690457341a773d0dc43b4631b1af7ff03
Piyush Shah <piyush720shah@gmail.com>
2026-07-22 17:26:29 +0530

    Add Valid Parentheses Implementation

---
+++ LinkedList/Valid parentheses.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the parentheses string: ");
+        String s = sc.nextLine();
+
+        if (isValid(s)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+
+        sc.close();
+    }
+}
